3d6366841f6ebb62ee33f61eae34fd4c78dc5ee5
James-Baek <ckdgus1428@gmail.com>
2019-07-06 17:44:53 +0900

    java 다시 시작

---
+++ test.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class test{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = 0;
+
+        for(int i = 0; i <= 10; ++i){
+            a = a + i;
+        }
+        System.out.println("test2");
+        System.out.println(a);
+    }
+}
